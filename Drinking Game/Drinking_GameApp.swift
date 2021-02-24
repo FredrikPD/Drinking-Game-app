17d51cc7897b6cf7e3f4c1bae589975c953b009c
Sauceyp <72051789+Sauceyp@users.noreply.github.com>
2021-02-24 12:57:34 +0100

    Initial Commit

---
+++ Drinking Game/Drinking_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drinking_GameApp.swift
+//  Drinking Game
+//
+//  Created by Fredrik Preus Dovland on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Drinking_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
